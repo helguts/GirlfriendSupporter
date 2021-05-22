01dda56b6e1af4f3c8efad65de130ac9f4c2d13c
helguts <j.hecht88@web.de>
2021-05-22 12:24:25 +0200

    Numbers of new posts are now shown on the buttons

--- app/src/main/java/com/example/girlfriendsupporter/MainActivity.kt
+++ app/src/main/java/com/example/girlfriendsupporter/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.girlfriendsupporter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.isVisible
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,23 +32,28 @@ class MainActivity : AppCompatActivity() {
         val likeButton = findViewById<Button>(R.id.button_like)
         val shareButton = findViewById<Button>(R.id.button_share)
         val checkButton = findViewById<Button>(R.id.button_check)
+        val newPostsCount = 5
         checkButton.setOnClickListener {
-            val checkedCount = 5
-            likeButton.isEnabled = checkedCount > 0
-            shareButton.isEnabled = checkedCount > 0
+            likeButton.isEnabled = newPostsCount > 0
+            likeButton.text = if (newPostsCount > 0) getString(R.string.like_x, newPostsCount) else getString(R.string.like)
+            shareButton.isEnabled = newPostsCount > 0
+            shareButton.text = if (newPostsCount > 0) getString(R.string.share_x, newPostsCount) else getString(R.string.share)
 
-            showNumberOfAffectedPosts(R.id.textview_posts_found, R.string.x_new, checkedCount)
+            val textLastChecked = findViewById<TextView>(R.id.textview_last_checked)
+            val formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
+            val currentTime = LocalDateTime.now().format(formatter)
+            textLastChecked.text = getString(R.string.last_checked, currentTime)
         }
 
         likeButton.setOnClickListener {
-            val likedCount = 4
-            showNumberOfAffectedPosts(R.id.textview_posts_liked, R.string.x_liked, likedCount)
+            showNumberOfAffectedPosts(R.id.textview_posts_liked, R.string.x_liked, newPostsCount)
+            likeButton.text = getString(R.string.like)
             likeButton.isEnabled = false
         }
 
         shareButton.setOnClickListener {
-            val sharedCount = 4
-            showNumberOfAffectedPosts(R.id.textview_posts_shared, R.string.x_shared, sharedCount)
+            showNumberOfAffectedPosts(R.id.textview_posts_shared, R.string.x_shared, newPostsCount)
+            shareButton.text = getString(R.string.share)
             shareButton.isEnabled = false
         }
     }
